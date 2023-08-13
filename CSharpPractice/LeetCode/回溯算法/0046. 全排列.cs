2b37df7ed5d3402de9b057c13ebac3fb4a4b7eff
yejinshengge <1085791632@qq.com>
2023-08-13 11:19:24 +0800

    LeetCode46.全排列、LeetCode491.递增子序列

---
+++ CSharpPractice/LeetCode/回溯算法/0046. 全排列.cs
@@ -0,0 +1,45 @@
+﻿using CSharpPractice.Util;
+
+namespace CSharpPractice.LeetCode.回溯算法;
+// 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+public class LeetCode_0046
+{
+    public static void Test()
+    {
+        LeetCode_0046 obj = new LeetCode_0046();
+        var list = obj.Permute(new[] { 1 });
+        Tools.PrintEnumerator(list);
+        
+    }
+    
+    public IList<IList<int>> Permute(int[] nums) {
+        _res.Clear();
+        _path.Clear();
+        _valid = new bool[21];
+        DoPermute(nums);
+        return _res;
+    }
+
+    private IList<IList<int>> _res = new List<IList<int>>();
+    private IList<int> _path = new List<int>();
+    private bool[] _valid ;
+    private void DoPermute(int[] nums)
+    {
+        if (_path.Count == nums.Length)
+        {
+            _res.Add(new List<int>(_path));
+            return;
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if(_valid[nums[i]+10])
+                continue;
+            _path.Add(nums[i]);
+            _valid[nums[i] + 10] = true;
+            DoPermute(nums);
+            _path.RemoveAt(_path.Count-1);
+            _valid[nums[i] + 10] = false;
+        }
+    }
+}
---
+++ CSharpPractice/LeetCode/回溯算法/0491. 递增子序列.cs
@@ -0,0 +1,42 @@
+﻿using CSharpPractice.Util;
+
+namespace CSharpPractice.LeetCode.回溯算法;
+
+/**
+给你一个整数数组 nums ，找出并返回所有该数组中不同的递增子序列，递增子序列中 至少有两个元素 。你可以按 任意顺序 返回答案。
+数组中可能含有重复元素，如出现两个整数相等，也可以视作递增序列的一种特殊情况。
+ */
+public class LeetCode_0491
+{
+    public static void Test()
+    {
+        LeetCode_0491 obj = new LeetCode_0491();
+        var list = obj.FindSubsequences(new[] { 4,4,3,2,1 });
+        Tools.PrintEnumerator(list);
+    }
+
+    public IList<IList<int>> FindSubsequences(int[] nums) {
+        _res.Clear();
+        _path.Clear();
+        DoFindSubsequences(nums,0);
+        return _res;
+    }
+
+    private IList<IList<int>> _res = new List<IList<int>>();
+    private IList<int> _path = new List<int>();
+    private void DoFindSubsequences(int[] nums, int startIndex)
+    {
+        if(_path.Count >= 2)
+            _res.Add(new List<int>(_path));
+        bool[] valid = new bool[201];
+        for (int i = startIndex; i < nums.Length; i++)
+        {
+            if((_path.Count > 0 && nums[i] < _path[^1]) || valid[nums[i]+100])
+                continue;
+            _path.Add(nums[i]);
+            valid[nums[i]+100] = true;
+            DoFindSubsequences(nums,i+1);
+            _path.RemoveAt(_path.Count-1);
+        }
+    }
+}
