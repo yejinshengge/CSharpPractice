802f71075fca3843c7bd65c923a38d29e32a2b55
yejinshengge <1085791632@qq.com>
2024-03-13 20:56:06 +0800

    0354. 俄罗斯套娃信封问题.cs

--- CSharpPractice/LeetCode/动态规划/序列动态规划/0354. 俄罗斯套娃信封问题.cs
+++ CSharpPractice/LeetCode/动态规划/序列动态规划/0354. 俄罗斯套娃信封问题.cs
@@ -7,8 +7,8 @@ public class LeetCode_0354
     public static void Test()
     {
         LeetCode_0354 obj = new LeetCode_0354();
-        Console.WriteLine(obj.MaxEnvelopes(Tools.ConstructTArray("[[5,4],[6,4],[6,7],[2,3]]")));
-        Console.WriteLine(obj.MaxEnvelopes(Tools.ConstructTArray("[[1,1],[1,1],[1,1]]")));
+        Console.WriteLine(obj.MaxEnvelopes2(Tools.ConstructTArray("[[5,4],[6,4],[6,7],[2,3]]")));
+        Console.WriteLine(obj.MaxEnvelopes2(Tools.ConstructTArray("[[1,1],[1,1],[1,1]]")));
     }
     
     public int MaxEnvelopes(int[][] envelopes)
@@ -37,5 +37,45 @@ public class LeetCode_0354
         }
 
         return max;
+    }
+
+    public int MaxEnvelopes2(int[][] envelopes)
+    {
+        Array.Sort(envelopes, (e1, e2) =>
+        {
+            if (e1[0] != e2[0])
+                return e1[0] - e2[0];
+            return e2[1] - e1[1];
+        });
+        return _lengthOfLIS(envelopes);
+    }
+
+    private int _lengthOfLIS(int[][] envelopes)
+    {
+        // 牌堆数
+        int piles = 0;
+        var length = envelopes.Length;
+        int[] top = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            int curNum = envelopes[i][1];
+            int left = 0;
+            int right = piles;
+            // 找到第一个牌顶比自己大的堆
+            while (left < right)
+            {
+                int mid = (left + right) >> 1;
+                if (top[mid] >= curNum)
+                    right = mid;
+                else
+                    left = mid + 1;
+            }
+            // 堆顶没有比当前大的牌,新建一个堆
+            if (left == piles) piles++;
+            top[left] = curNum;
+        }
+
+        return piles;
     }
 }
