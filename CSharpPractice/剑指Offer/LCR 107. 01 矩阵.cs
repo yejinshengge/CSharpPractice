0d270c2d9445bfa2258640048d6594a48f685309
yejinshengge <1085791632@qq.com>
2024-11-30 00:11:20 +0800

    LCR 107. 01 矩阵.cs LCR 108. 单词接龙.cs

---
+++ CSharpPractice/剑指Offer/LCR 107. 01 矩阵.cs
@@ -0,0 +1,54 @@
+using CSharpPractice.Util;
+
+namespace CSharpPractice.剑指Offer;
+
+public class LeetCode_LCR107
+{
+    public static void Test()
+    {
+        LeetCode_LCR107 obj = new LeetCode_LCR107();
+        Tools.PrintArr(obj.UpdateMatrix(Tools.ConstructTArray("[[0,0,0],[0,1,0],[0,0,0]]")));
+        Tools.PrintArr(obj.UpdateMatrix(Tools.ConstructTArray("[[0,0,0],[0,1,0],[1,1,1]]")));
+        Tools.PrintArr(obj.UpdateMatrix(Tools.ConstructTArray("[[0]]")));
+        Tools.PrintArr(obj.UpdateMatrix(Tools.ConstructTArray("[[1,1,0,1,1]]")));
+    }
+    
+    public int[][] UpdateMatrix(int[][] mat)
+    {
+        int[][] res = new int[mat.Length][];
+        Queue<int[]> queue = new Queue<int[]>();
+        for (var i = 0; i < res.Length; i++)
+        {
+            res[i] = new int[mat[i].Length];
+            for (int j = 0; j < res[0].Length; j++)
+            {
+                if (mat[i][j] != 0)
+                    res[i][j] = int.MaxValue;
+                else
+                    queue.Enqueue(new []{i,j});
+            }
+        }
+
+        int[][] dirs = new[] { new int[] { 1, 0 }, new[] { -1, 0 }, new[] { 0, 1 }, new[] { 0, -1 } };
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            int distance = res[node[0]][node[1]];
+            foreach (var dir in dirs)
+            {
+                int row = node[0] + dir[0];
+                int col = node[1] + dir[1];
+                if (row >= 0 && row < mat.Length && col >= 0 && col < mat[0].Length)
+                {
+                    if (res[row][col] > distance + 1)
+                    {
+                        res[row][col] = distance + 1;
+                        queue.Enqueue(new []{row,col});
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
---
+++ CSharpPractice/剑指Offer/LCR 108. 单词接龙.cs
@@ -0,0 +1,138 @@
+namespace CSharpPractice.剑指Offer;
+
+public class LeetCode_LCR108
+{
+    public static void Test()
+    {
+        LeetCode_LCR108 obj = new LeetCode_LCR108();
+        Console.WriteLine(obj.LadderLength("hit","cog",new List<string>(){"hot","dot","dog","lot","log","cog"}));
+        Console.WriteLine(obj.LadderLength("hit","cog",new List<string>(){"hot","dot","dog","lot","log"}));
+        Console.WriteLine(obj.LadderLength("a","c",new List<string>(){"a","b","c"}));
+    }
+    
+    public int LadderLength1(string beginWord, string endWord, IList<string> wordList)
+    {
+        Dictionary<string, List<string>> dic = new();
+        for (var i = 0; i < wordList.Count; i++)
+        {
+            var word1 = wordList[i];
+            dic.Add(word1,new List<string>());
+            for (int j = 0; j < wordList.Count; j++)
+            {
+                if (i == j) continue;
+                var word2 = wordList[j];
+                if (_checkCanTrans(word1, word2))
+                    dic[word1].Add(word2);
+            }
+        }
+
+        if (!dic.ContainsKey(endWord)) return 0;
+        if (!dic.ContainsKey(beginWord))
+        {
+            dic.Add(beginWord,new List<string>());
+            for (int i = 0; i < wordList.Count; i++)
+            {
+                if(_checkCanTrans(beginWord,wordList[i]))
+                    dic[beginWord].Add(wordList[i]);
+            }
+        }
+
+        Queue<string> queue = new();
+        HashSet<string> visited = new();
+        int len = 0;
+        queue.Enqueue(beginWord);
+        while (queue.Count > 0)
+        {
+            len++;
+            var count = queue.Count;
+            for (int j = 0; j < count; j++)
+            {
+                var curWord = queue.Dequeue();
+                visited.Add(curWord);
+                var list = dic[curWord];
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i] == endWord) return ++len;
+                    if(visited.Contains(list[i])) continue;
+                    queue.Enqueue(list[i]);
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private bool _checkCanTrans(string str1, string str2)
+    {
+        bool flag = false;
+        for (int i = 0; i < str1.Length; i++)
+        {
+            if (str1[i] == str2[i]) continue;
+            if (flag) return false;
+            flag = true;
+        }
+        return true;
+    }
+    
+    public int LadderLength(string beginWord, string endWord, IList<string> wordList)
+    {
+        Dictionary<string, List<string>> dic = new();
+        for (var i = 0; i < wordList.Count; i++)
+        {
+            var word1 = wordList[i];
+            dic.Add(word1,new List<string>());
+            for (int j = 0; j < wordList.Count; j++)
+            {
+                if (i == j) continue;
+                var word2 = wordList[j];
+                if (_checkCanTrans(word1, word2))
+                    dic[word1].Add(word2);
+            }
+        }
+
+        if (!dic.ContainsKey(endWord)) return 0;
+        if (!dic.ContainsKey(beginWord))
+        {
+            dic.Add(beginWord,new List<string>());
+            for (int i = 0; i < wordList.Count; i++)
+            {
+                if(_checkCanTrans(beginWord,wordList[i]))
+                    dic[beginWord].Add(wordList[i]);
+            }
+        }
+
+        HashSet<string> beginSet = new();
+        HashSet<string> endSet = new();
+        HashSet<string> visited = new();
+        int len = 2;
+        beginSet.Add(beginWord);
+        endSet.Add(endWord);
+        while (beginSet.Count > 0 && endSet.Count > 0)
+        {
+            // 始终查找最少的
+            if (beginSet.Count > endSet.Count)
+                (beginSet, endSet) = (endSet, beginSet);
+            
+            
+            HashSet<string> tmp = new HashSet<string>();
+            foreach (var str in beginSet)
+            {
+                var list = dic[str];
+                visited.Add(str);
+                foreach (var neighbor in list)
+                {
+                    // 相遇
+                    if (endSet.Contains(neighbor)) return len;
+                    // 已查询过
+                    if(visited.Contains(neighbor)) continue;
+                    tmp.Add(neighbor);
+                }
+            }
+
+            beginSet = tmp;
+            len++;
+        }
+
+        return 0;
+    }
+}
