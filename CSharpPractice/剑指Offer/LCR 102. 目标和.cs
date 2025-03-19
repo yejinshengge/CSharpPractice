7be431c8e549b2b62dfe0dccec5bc3e24ef81802
yejinshengge <1085791632@qq.com>
2025-03-19 13:23:30 +0800

    复习

--- CSharpPractice/剑指Offer/LCR 102. 目标和.cs
+++ CSharpPractice/剑指Offer/LCR 102. 目标和.cs
@@ -12,6 +12,7 @@ public class LeetCode_LCR102
     
     public int FindTargetSumWays(int[] nums, int target)
     {
+        // 找到可以凑成( target + sum ) / 2的方法
         int sum = 0;
         for (var i = 0; i < nums.Length; i++)
         {
@@ -20,7 +21,11 @@ public class LeetCode_LCR102
         sum = (sum + target);
         if (sum % 2 != 0) return 0;
         sum /= 2;
+        // 设dp[i][j]为前i个数凑成j的方法数
+        // 状态转移方程：dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i]]
+        // 优化成滚动数组：dp[j] = dp[j] + dp[j-nums[i]]
         int[] dp = new int[sum+1];
+        // 前0个数凑成0的方法数为1
         dp[0] = 1;
         for (int i = 1; i <= nums.Length; i++)
         {
--- CSharpPractice/剑指Offer/LCR 104. 组合总和 Ⅳ.cs
+++ CSharpPractice/剑指Offer/LCR 104. 组合总和 Ⅳ.cs
@@ -12,8 +12,11 @@ public class LeetCode_LCR104
     
     public int CombinationSum4(int[] nums, int target)
     {
+        // 设dp[i]为凑成i的排列数
         int[] dp = new int[target+1];
+        // 凑成0的排列数为1
         dp[0] = 1;
+        // 先枚举目标值，再枚举硬币，相当于不固定硬币的顺序
         for (int i = 1; i <= target; i++)
         {
             for (int j = 0; j < nums.Length; j++)
